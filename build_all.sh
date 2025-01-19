374190feb51318a087379a14b59b735d235d5688
Nghia Ho <nghiaho12@yahoo.com>
2025-01-19 15:51:57 -0800

    refactor build release

--- build_all.sh
+++ build_all.sh
@@ -12,5 +12,5 @@ docker build -f Dockerfile.wasm -t number_sequence:wasm .
 docker run --rm -it -v /tmp:/tmp --network=host number_sequence:wasm /bin/bash -c 'cp /number_sequence/build/release/* /tmp'
 
 docker build -f Dockerfile.android -t number_sequence:android .
-docker run --rm -it -v /tmp:/tmp --network=host number_sequence:android /bin/bash -c 'cp /SDL/build/org.libsdl.number_sequence/app/build/outputs/apk/debug/*.apk /tmp'
+docker run --rm -it -v /tmp:/tmp --network=host number_sequence:android /bin/bash -c 'cp /SDL/build/org.libsdl.number_sequence/app/build/outputs/apk/release/*.apk /tmp'
 
