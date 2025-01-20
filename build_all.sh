99fc89624133c56051b0fecad39b7659bd7f0199
Nghia Ho <nghiaho12@yahoo.com>
2025-01-19 17:40:49 -0800

    refactor for build release

--- build_all.sh
+++ build_all.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-docker build -f Dockerfile.linux -t number_sequence:linux .
-docker run --rm -it -v /tmp:/tmp --network=host number_sequence:linux /bin/bash -c 'cp /number_sequence/build/release/* /tmp'
+docker build -f Dockerfile.linux -t number_sequence_game:linux .
+docker run --rm -it -v /tmp:/tmp --network=host number_sequence_game:linux /bin/bash -c 'cp /number_sequence_game/build/release/* /tmp'
 
-docker build -f Dockerfile.windows -t number_sequence:windows .
-docker run --rm -it -v /tmp:/tmp --network=host number_sequence:windows /bin/bash -c 'cp /number_sequence/build/release/* /tmp'
+docker build -f Dockerfile.windows -t number_sequence_game:windows .
+docker run --rm -it -v /tmp:/tmp --network=host number_sequence_game:windows /bin/bash -c 'cp /number_sequence_game/build/release/* /tmp'
 
-docker build -f Dockerfile.wasm -t number_sequence:wasm .
-docker run --rm -it -v /tmp:/tmp --network=host number_sequence:wasm /bin/bash -c 'cp /number_sequence/build/release/* /tmp'
+docker build -f Dockerfile.wasm -t number_sequence_game:wasm .
+docker run --rm -it -v /tmp:/tmp --network=host number_sequence_game:wasm /bin/bash -c 'cp /number_sequence_game/build/release/* /tmp'
 
-docker build -f Dockerfile.android -t number_sequence:android .
-docker run --rm -it -v /tmp:/tmp --network=host number_sequence:android /bin/bash -c 'cp /SDL/build/org.libsdl.number_sequence/app/build/outputs/apk/release/*.apk /tmp'
+docker build -f Dockerfile.android -t number_sequence_game:android .
+docker run --rm -it -v /tmp:/tmp --network=host number_sequence_game:android /bin/bash -c 'cp /SDL/build/org.libsdl.number_sequence_game/app/build/outputs/apk/release/*.apk /tmp'
 
