0f73e3690539ed5ee8eb6cc415966ec5178c8ca9
Nghia Ho <nghiaho12@yahoo.com>
2025-01-26 17:19:08 -0800

    revert back to debug build for Android APK, can't run unsigned release on phone

--- build_all.sh
+++ build_all.sh
@@ -12,5 +12,5 @@ docker build -f Dockerfile.wasm -t number_sequence_game:wasm .
 docker run --rm -it -v /tmp:/tmp --network=host number_sequence_game:wasm /bin/bash -c 'cp /number_sequence_game/build/release/* /tmp'
 
 docker build -f Dockerfile.android -t number_sequence_game:android .
-docker run --rm -it -v /tmp:/tmp --network=host number_sequence_game:android /bin/bash -c 'cp /SDL/build/org.libsdl.number_sequence_game/app/build/outputs/apk/release/*.apk /tmp'
+docker run --rm -it -v /tmp:/tmp --network=host number_sequence_game:android /bin/bash -c 'cp /SDL/build/org.libsdl.number_sequence_game/app/build/outputs/apk/debug/*.apk /tmp'
 
